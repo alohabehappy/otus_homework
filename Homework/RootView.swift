153128716ff5f753faba2b8a14b3909cf98efb7c
Vadim Roshchin <alohabehappy@gmail.com>
2019-12-03 20:12:31 +0300

    initial commit, added xcode project

---
+++ Homework/RootView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Homework
+//
+//  Created by Vadim on 03.12.2019.
+//  Copyright © 2019 alohabehappy. All rights reserved.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
---
+++ Homework/SceneDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  SceneDelegate.swift
+//  Homework
+//
+//  Created by Vadim on 03.12.2019.
+//  Copyright © 2019 alohabehappy. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        let rootView = RootView()
+
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: rootView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+
+}
